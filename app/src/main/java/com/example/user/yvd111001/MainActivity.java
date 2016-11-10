11dbbc84989588d13ee0db08667e58bde1d21543
Shaojung <shaojung@gmail.com>
2016-11-10 11:22:27 +0800

    Custom Layout by program..

--- app/src/main/java/com/example/user/yvd111001/MainActivity.java
+++ app/src/main/java/com/example/user/yvd111001/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.user.yvd111001;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutCompat;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -11,15 +12,24 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.layout2);
 
-        LinearLayout ll = (LinearLayout) findViewById(R.id.layout2_linear);
+
+        LinearLayout ll = new LinearLayout(MainActivity.this);
+        // ll.setOrientation(LinearLayout.VERTICAL);
+        ll.setOrientation(LinearLayout.HORIZONTAL);
+
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
+                ViewGroup.LayoutParams.WRAP_CONTENT, 1);
+
         Button btn = new Button(MainActivity.this);
         btn.setText("Button!!");
+        btn.setLayoutParams(params);
         ll.addView(btn);
         Button btn2 = new Button(MainActivity.this);
+        btn2.setLayoutParams(params);
         btn2.setText("Button 22 !!");
         ll.addView(btn2);
+        setContentView(ll);
 //        Button btn = new Button(MainActivity.this);
 //        btn.setText("Click me!");
 //        setContentView(btn);
